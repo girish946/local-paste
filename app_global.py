9fb33774b44caa896565e1b98899617be6303eb1
Girish Joshi <girish946@gmail.com>
2018-09-22 01:14:01 +0530

    db shifted to ~/.local-paste

--- app_global.py
+++ app_global.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from os.path import expanduser
+home = expanduser("~")
 
 # init flask app
 app     = Flask(__name__)
 
 # sqlite DB file
-DB_FILE = 'localPaste.db'
+DB_FILE = home+'/.local-paste/localPaste.db'
