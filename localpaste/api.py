afae981e059a7daa316f232a7a7d4cc93de9942e
Girish Joshi <girish946@gmail.com>
2019-07-13 23:16:30 +0530

    used black for linting

--- localpaste/api.py
+++ localpaste/api.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from .dbconnect import (createTables, insertPaste, selectDb,
-                        deletePaste, updatePaste, selectPaste,
-                        searchPaste, getLogin)
+from .dbconnect import (
+    createTables,
+    insertPaste,
+    selectDb,
+    deletePaste,
+    updatePaste,
+    selectPaste,
+    searchPaste,
+    getLogin,
+)
 from flask import request, session
 from flask_restful import Resource
 from .app_global import config
@@ -14,6 +21,7 @@ class DbInit(Resource):
     This class is responsible for creating the dbatabase.
     The first thing to be done after setup is to create a db.
     """
+
     def get(self):
         if createTables():
             return {"CreateDb": "Success"}
@@ -29,13 +37,12 @@ class NewPaste(Resource):
     --data '{"name": "Some PasteName", "content": "dummy Content"}' \
     http://localhost:5000/api/new
     """
-    def put(self):
-        name = request.json['name']
-        content = request.json['content']
 
-        insertPaste(name=name,
-                    content=content,
-                    filename=name+'.txt')
+    def put(self):
+        name = request.json["name"]
+        content = request.json["content"]
+
+        insertPaste(name=name, content=content, filename=name + ".txt")
         return {"insert": "success"}
 
 
@@ -44,6 +51,7 @@ class DeletePaste(Resource):
     To delete the paste.
     curl http://localhost:5000/api/delete/<pasteId> -X DELET
     """
+
     def delete(self, pasteId):
         """
         Only admin should be able to delete the paste.
@@ -66,13 +74,18 @@ class UpdatePaste(Resource):
     --data '{"name": "New PasteName", "content": " newdummy Content"}' \
     http://localhost:5000/api/update/<pasteId>
     """
+
     def put(self, pasteId):
-        name = request.json['name']
-        content = request.json['content']
+        name = request.json["name"]
+        content = request.json["content"]
         # print(name, content)
-        updatePaste(pasteId=pasteId, pasteName=name,
-                    pasteContent=content, fileName=name+".txt",
-                    debug=True)
+        updatePaste(
+            pasteId=pasteId,
+            pasteName=name,
+            pasteContent=content,
+            fileName=name + ".txt",
+            debug=True,
+        )
         return {"ok": "done"}
 
 
@@ -81,31 +94,40 @@ class GetPaste(Resource):
     To retrive a paste.
     curl http://localhost:5000/api/get/<pasteId>
     """
+
     def get(self, pasteId):
         paste = [i for i in selectPaste(pasteId=pasteId)]
         if paste:
-            return {"Name": paste[0].Name, "Id": paste[0].Id.hex,
-                    "Content": paste[0].Content,
-                    "TimeStamp": paste[0].TimeStamp.strftime("%b %d %Y %H:%M:%S")}
+            return {
+                "Name": paste[0].Name,
+                "Id": paste[0].Id.hex,
+                "Content": paste[0].Content,
+                "TimeStamp": paste[0].TimeStamp.strftime("%b %d %Y %H:%M:%S"),
+            }
         else:
             return {"Error": "No such paste"}
+
 
 class SearchPaste(Resource):
     """
     To search a keyword in the pastes.
     curl http://localhost:5000/api/search/<keyword>
     """
+
     def get(self, keyword=None):
-        pastes = [{"Id": i.Id.hex, "Name": i.Name,
-                   "Content": i.Content,
-                   "TimeStamp": i.TimeStamp.strftime("%b %d %Y %H:%M:%S")
-                   }
-                  for i in searchPaste(keyword=keyword)
-                  ]
+        pastes = [
+            {
+                "Id": i.Id.hex,
+                "Name": i.Name,
+                "Content": i.Content,
+                "TimeStamp": i.TimeStamp.strftime("%b %d %Y %H:%M:%S"),
+            }
+            for i in searchPaste(keyword=keyword)
+        ]
         return {keyword: pastes}
 
     def post(self):
-        keyword = request.form['keyword']
+        keyword = request.form["keyword"]
         return self.get(keyword=keyword)
 
 
@@ -113,14 +135,18 @@ class SelectNextX(Resource):
     """
     To select the next x pastes. The default is 10
     """
+
     def get(self, limit=10, offset=0):
         try:
-            pastes = [{"Id": i.Id.hex, "Name": i.Name,
-                       "Content": i.Content,
-                       "TimeStamp": i.TimeStamp.strftime("%b %d %Y %H:%M:%S")
-                       }
-                      for i in selectDb(limit=limit, offset=offset)
-                      ]
+            pastes = [
+                {
+                    "Id": i.Id.hex,
+                    "Name": i.Name,
+                    "Content": i.Content,
+                    "TimeStamp": i.TimeStamp.strftime("%b %d %Y %H:%M:%S"),
+                }
+                for i in selectDb(limit=limit, offset=offset)
+            ]
             return {"pastes": pastes}
         except Exception as e:
             return {"Error": str(e)}
@@ -131,14 +157,18 @@ class SelectDb(Resource):
     To retrive all pastes from the db.
     curl http://localhost:5000/api/selectDb
     """
+
     def get(self):
         try:
-            pastes = [{"Id": i.Id.hex, "Name": i.Name,
-                       "Content": i.Content,
-                       "TimeStamp": i.TimeStamp.strftime("%b %d %Y %H:%M:%S")
-                       }
-                      for i in selectDb(limit=0)
-                      ]
+            pastes = [
+                {
+                    "Id": i.Id.hex,
+                    "Name": i.Name,
+                    "Content": i.Content,
+                    "TimeStamp": i.TimeStamp.strftime("%b %d %Y %H:%M:%S"),
+                }
+                for i in selectDb(limit=0)
+            ]
             return {"pastes": pastes}
         except Exception as e:
             return {"Error": str(e)}
@@ -148,6 +178,7 @@ class UserLogin(Resource):
     """
     user login
     """
+
     def post(self):
         # print(request.json)
         if "username" in request.json:
@@ -156,24 +187,24 @@ class UserLogin(Resource):
                 password = request.json["password"]
                 # print(request.json)
                 if getLogin(username, password):
-                    session['username'] = username
-                    session['token'] = config['admin_session']
-                    return {"login": "success",
-                            "token": config["admin_session"]}
+                    session["username"] = username
+                    session["token"] = config["admin_session"]
+                    return {"login": "success", "token": config["admin_session"]}
                 else:
-                    return{"login": "failed"}
+                    return {"login": "failed"}
 
 
 class UserLogout(Resource):
     """
     user logout
     """
+
     def get(self):
-        if 'username' in session:
-            session.pop('username')
-            config.pop('admin_session')
+        if "username" in session:
+            session.pop("username")
+            config.pop("admin_session")
             print(session)
             print(config)
             return {"logout": "success"}
         else:
-            return{"logout": "failed"}
+            return {"logout": "failed"}
